30f69743f197cd425da0cc0abfd6ec04460b99f9
Daianrj <daian.operacional@gmail.com>
2026-09-22 18:42:34 -0300

    chore: version showroom modules on demos/sheet-system.js

--- demos/sheet-system.js
+++ demos/sheet-system.js
@@ -1,4 +1,4 @@
-import {wait,esc} from './shared.js';
+import {wait,esc} from './shared.js?v=20260922-showroom3';
 
 const stages=['Planilha','Automação','Banco / Dados','Web App','Dashboard','Sistema Operacional'];
 let rootRef=null,stage=0,running=false,guideText='',guideToken=0;
