ee69527991f5424e39f5d558d28165f06f8ab64a
Daianrj <daian.operacional@gmail.com>
2026-09-22 18:52:53 -0300

    chore: bump demonstrations 2.1 final cache on demos/stock.js

--- demos/stock.js
+++ demos/stock.js
@@ -1,4 +1,4 @@
-import {clone,esc,uid,barChart,wait} from './shared.js?v=20260922-showroom4';
+import {clone,esc,uid,barChart,wait} from './shared.js?v=20260922-showroom5';
 const initial=[
 {id:'SKU-1001',name:'Caixa reforçada 40x30',category:'Embalagens',stock:36,min:20},
 {id:'SKU-1002',name:'Filme stretch 50cm',category:'Embalagens',stock:23,min:18},
